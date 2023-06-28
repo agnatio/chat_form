b7b3edd678610984a75a975b08e5542a0354c4e2
agnatio <agnatio11@gmail.com>
2023-06-28 22:19:46 +0300

    new version added with window scalability and menu item

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QLineEdit, QTextEdit, QDialog, QVBoxLayout, QFileDialog
-from PyQt5.QtCore import Qt
 import os
-import sys
-from datetime import datetime
 import json
+from datetime import datetime
+from PyQt5.QtCore import QCoreApplication, QMetaObject, QRect, QSize, QUrl, Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QFileDialog, QHBoxLayout, QLabel, QLineEdit, QMainWindow, QMenuBar, \
+    QPushButton, QStatusBar, QTextEdit, QVBoxLayout, QWidget, QAction, QDialog, QGridLayout, QMessageBox, QMenu
 
-class NameDialog(QDialog):
+class Authorisation(QDialog):
     def __init__(self):
         super().__init__()
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.icon = QIcon(os.path.join(self.current_dir, "fox.ico"))
+
         self.setWindowTitle("Name Entry")
         self.setFixedSize(150, 100)
+        self.setWindowIcon(self.icon)
 
         layout = QVBoxLayout()
 
@@ -18,7 +22,7 @@ class NameDialog(QDialog):
         layout.addWidget(self.label)
 
         self.name_field = QLineEdit()
-        self.name_field.returnPressed.connect(self.on_submit)  # Connect Enter key press to on_submit method
+        self.name_field.returnPressed.connect(self.on_submit)
         layout.addWidget(self.name_field)
 
         self.submit_button = QPushButton("Submit")
@@ -27,62 +31,105 @@ class NameDialog(QDialog):
 
         self.setLayout(layout)
 
-       
-
     def on_submit(self):
         name = self.name_field.text()
         if name:
             self.accept()
 
-class ChatWindow(QMainWindow):
+class Ui_MainWindow(object):
     def __init__(self):
-        super().__init__()
-        self.setGeometry(200, 200, 500, 400)
-        self.setWindowTitle("Chat Window")
-        # block resizing of the window
-        self.setFixedSize(self.size())
-        self.chat_history_folder = "chat_history"
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.chat_history_folder = os.path.join(self.current_dir, "chat_history")
+        self.icon = QIcon(os.path.join(self.current_dir, "fox.ico"))
 
-        self.label = QLabel(self)
-        self.label.move(50, 50)
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(675, 570)
+        MainWindow.showEvent = self.on_main_window_show
+        MainWindow.setWindowIcon(self.icon)
+        MainWindow.setWindowTitle("MESSENGER")
+        self.actionDownload_messages = QAction(MainWindow)
+        self.actionDownload_messages.setObjectName("actionDownload_messages")
+        self.actionDownload_messages.triggered.connect(self.upload_chat_history)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.textEdit = QTextEdit(self.centralwidget)
+        self.textEdit.setObjectName("textEdit")
+        self.textEdit.setReadOnly(True)
 
-        self.text_field = QLineEdit(self)
-        self.text_field.move(50, 300)
-        self.text_field.resize(300, 30)
-        self.text_field.returnPressed.connect(self.on_button_clicked)  # Connect Enter key press to on_button_clicked method
+        self.gridLayout.addWidget(self.textEdit, 0, 0, 1, 1)
 
-        self.text_area = QTextEdit(self)
-        self.text_area.move(50, 100)
-        self.text_area.resize(400, 180)
-        self.text_area.setReadOnly(True)
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName("label")
 
-        self.button = QPushButton(self)
-        self.button.setText("Send")
-        self.button.move(250, 300)
-        self.button.clicked.connect(self.on_button_clicked)
+        self.horizontalLayout.addWidget(self.label)
 
-        self.upload_button = QPushButton(self)
-        self.upload_button.setText("Upload History")
-        self.upload_button.move(360, 300)
-        self.upload_button.clicked.connect(self.upload_chat_history)
+        self.lineEdit = QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName("lineEdit")
 
+        self.horizontalLayout.addWidget(self.lineEdit)
+
+        self.Send = QPushButton(self.centralwidget)
+        self.Send.setObjectName("Send")
+        self.Send.setMinimumSize(QSize(100, 30))
+        self.Send.setMaximumSize(QSize(200, 30))
+        self.Send.clicked.connect(self.on_button_clicked)
+        self.lineEdit.returnPressed.connect(self.Send.click)
+
+        self.horizontalLayout.addWidget(self.Send)
+
+        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName("menubar")
+        self.menubar.setGeometry(QRect(0, 0, 675, 26))
+        self.menuChat = QMenu(self.menubar)
+        self.menuChat.setObjectName("menuChat")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuChat.menuAction())
+        self.menuChat.addAction(self.actionDownload_messages)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
 
         self.user_name = ""
         self.chat_history = []
 
         self.show_name_dialog()
 
+    def on_main_window_show(self, event):
+        self.lineEdit.setFocus(Qt.OtherFocusReason)
+        event.accept()
+
     def show_name_dialog(self):
-        dialog = NameDialog()
+        dialog = Authorisation()
         if dialog.exec_() == QDialog.Accepted:
             self.user_name = dialog.name_field.text()
             self.label.setText(self.user_name)
         else:
             sys.exit()
 
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", "MainWindow", None))
+        self.actionDownload_messages.setText(QCoreApplication.translate("MainWindow", "Download messages", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", "TextLabel", None))
+        self.Send.setText(QCoreApplication.translate("MainWindow", "Send", None))
+        self.menuChat.setTitle(QCoreApplication.translate("MainWindow", "Chat", None))
 
     def on_button_clicked(self):
-        text = self.text_field.text()
+        print('PyQt5 button click')
+        text = self.lineEdit.text()
         if text:
             current_time = datetime.now().strftime("%d.%m.%y|%H:%M:%S")
             message = {
@@ -90,23 +137,29 @@ class ChatWindow(QMainWindow):
                 'user': self.user_name,
                 'message': text
             }
+            print(message)
             self.chat_history.append(message)
-            self.text_field.clear()
+            self.lineEdit.clear()
             self.save_chat_history()
             self.update_chat_history()
 
     def save_chat_history(self):
-        filename = f"{self.chat_history_folder}/{self.user_name}_chat_history.json"
+        filename = os.path.join(self.chat_history_folder, f"{self.user_name}_chat_history.json")
         user_chat_history = [message for message in self.chat_history if message['user'] == self.user_name]
         with open(filename, 'w') as file:
             json.dump(user_chat_history, file, indent=4)
 
-    def closeEvent(self, event):
-        self.save_chat_history()
-        event.accept()
+    def update_chat_history(self):
+        self.textEdit.clear()
+        sorted_history = sorted(self.chat_history, key=lambda x: x['timestamp'])
+        for message in sorted_history:
+            timestamp = message['timestamp']
+            user = message['user']
+            text = message['message']
+            self.textEdit.append(f"{timestamp} - {user}: {text}")
 
     def upload_chat_history(self):
-        folder_path = QFileDialog.getExistingDirectory(self, "Select Folder")
+        folder_path = QFileDialog.getExistingDirectory(None, "Select Folder")
         if folder_path:
             self.chat_history = []
             for file_name in os.listdir(folder_path):
@@ -117,20 +170,11 @@ class ChatWindow(QMainWindow):
                         self.chat_history.extend(chat_data)
             self.update_chat_history()
 
-
-    def update_chat_history(self):
-        self.text_area.clear()
-        sorted_history = sorted(self.chat_history, key=lambda x: x['timestamp'])
-        for message in sorted_history:
-            timestamp = message['timestamp']
-            user = message['user']
-            text = message['message']
-            self.text_area.append(f"{timestamp} - {user}: {text}")
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import sys
     app = QApplication(sys.argv)
-    chat_window = ChatWindow()
-    chat_window.show()
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
